4693e187e0a42c39f22597ed6c1a7ec3bb6cbbc1
Natalya Ch <n_babaeva@mail.ru>
2022-09-23 13:55:59 +0300

    initial commit

---
+++ ex41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+
+int[] InputArrayElements()
+{
+    Console.WriteLine("Введите любое количество чисел:");
+    string line = Console.ReadLine()!;
+    string[] parts = line.Split(' ');
+    int[] numbers = new int[parts.Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = int.Parse(parts[i]);
+    }
+    return numbers;
+}
+
+int CountPositivNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"Положительных чисел : {CountPositivNumber(InputArrayElements())}");
---
+++ ex43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+
+
+
+
+Console.WriteLine("Введите число k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+
+
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Пересечение в точке: ({Math.Round(x, 3)};{Math.Round(y, 3)})");
+}
